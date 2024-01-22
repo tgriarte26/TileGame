96a72c635df9b3520d2bb33369cebb35954f7b87
tgriarte <trevorgriarte@gmail.com>
2024-01-22 09:39:03 -0800

    first commit

---
+++ core/src/com/bmhs/game/gfx/assets/screens/TitleScreen.java
@@ -0,0 +1,39 @@
+package com.bmhs.game.gfx.assets.screens;
+
+import com.badlogic.gdx.Screen;
+import com.bmhs.game.GameHandler;
+
+public class TitleScreen implements Screen {
+
+    GameHandler game;
+    public TitleScreen(final GameHandler game) {
+        this.game = game;
+    }
+    public void show(){
+
+    }
+    public void render(float delta) {
+
+    }
+    public void resize(int width, int height) {
+
+    }
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
